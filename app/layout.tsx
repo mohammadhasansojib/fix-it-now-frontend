2e9ab0b81031d6023aa9ecfdb020572da3a9a879
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-08-03 16:01:58 +0600

    design: make main content min height full screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,11 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        {children}
+
+        <main className="flex-1">
+          {children}
+        </main>
+
         <Footer />
 
         <Toaster position="top-right" richColors />
